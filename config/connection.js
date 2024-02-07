606f7078efd3112e723cbd18b3cfe2d1be4bacd5
Aquib Hussain <128388823+aquibhussain12@users.noreply.github.com>
2024-02-07 21:16:39 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 
-mongoose.connect('mongodb+srv://aquibaquibdar12:aquibg%4012@mongomuslim.5q97xok.mongodb.net/Hadith');
+mongoose.connect('process.env.MONGO_URI');
 const conn =mongoose.connection;
 conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
 conn.once('open', () => {
