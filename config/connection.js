ff706166435468f118879a2b0313a9044f45e727
Aquib Hussain <128388823+aquibhussain12@users.noreply.github.com>
2024-02-07 21:20:18 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 
-mongoose.connect('process.env.MONGO_URI');
+mongoose.connect(process.env.MONGO_URI);
 const conn =mongoose.connection;
 conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
 conn.once('open', () => {
